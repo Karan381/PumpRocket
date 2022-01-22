b6965e948b862387d3d939bf9cb0b4b64a15e7d1
Karan Gupta <kg042632@gmail.com>
2022-01-22 13:38:12 +0530

    level loading crash detecter

---
+++ Assets/Scripts/CollissionHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollissionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("this is friendly");
+                break;
+            case "Finish":
+                NextLevel();
+                break;
+            case "Fuel":
+                Debug.Log("fuel");
+                break;
+            default:
+                ReloadScene();
+                break;
+
+        }
+    }
+
+    private static void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
+
